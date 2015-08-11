998c9e3fe24ceb40bae2684472465e40bae5f625
chenRiang <chenriang@gmail.com>
2015-08-11 10:27:40 +0800

    Second Error Commit

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello mate,im here just to try jenkins");
+            Console.WriteLine("Hello mate,im here just to try jenkins")
             justPrint();
             justTakeInt(858);
 
